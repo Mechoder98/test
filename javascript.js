6077fe65079650a27ae835324a3ec290fa9b3065
Ranjan <ranjukn98@gmail.com>
2022-01-26 20:25:52 +0530

    mini program for addition and multiplication

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let x, y, z, sum;
+
+x = 5;
+
+y = 7;
+
+z = 9;
+
+sum = (x + y) / z;
+
+console.log("product of x, y, and z is " +sum)
+
+// tiny program for multiplication
+
+var a, b, product;
+
+a = 10;
+
+b = 5;
+
+product = a*b;
+
+console.log("product of a and b is " +product)
