3694abcc1f4931ed95a350961b60b187c87819c1
Ranjan <ranjukn98@gmail.com>
2022-01-29 17:29:14 +0530

    29-jan-2022

---
+++ javascript-2.js
@@ -0,0 +1,46 @@
+// function addition(){
+
+    /* let a, b, c, sum;
+
+    a = 5;
+
+    b = 6;
+
+    c = 8;
+
+    sum = (a+b)*c;
+    
+    // return sum;
+
+    console.log("sum is " +sum);
+
+
+}
+
+
+addition(); */
+
+
+ 
+
+
+
+
+var input;
+input = 1;
+
+if ( input === undefined) {
+
+    console.log("satisfied");
+}
+
+else{
+
+    console.log("not satisfied");
+}
+
+
+
+
+
+
--- somecode.js
+++ somecode.js
@@ -16,3 +16,62 @@ let sent1 = "Nice car ";
 sent1 += "is in the city";
 
 console.log("Neha said: " +sent1)
+
+console.log(5 | 2);
+
+// subtraction
+
+var a, b, c;
+
+a = 23;
+
+b = 15;
+
+c = a - b;
+
+console.log(" value of c is " +c);
+
+// MAth.pow function
+
+let r = 5;
+
+let s = Math.pow(r, 3);
+
+console.log("s is " +s);
+
+
+// Arrays
+
+const cars = ["Maruthi", "Hyundai", "Honda", "Tata"];
+
+console.log(cars [0]);
+
+// Objects
+
+const vehicle = { 
+    
+    Brand: "Porshe",
+    model: " Carrera 911 turbo",
+    Engine_displacement: "3200cc",
+    Number_of_cylinders: " 4 cylinder "
+};
+
+console.log(vehicle.Brand + vehicle.model + " has a " + vehicle.Engine_displacement + vehicle.Number_of_cylinders + " turbo charge Engine");
+
+// using function and return
+
+function Myfunction (f1, f2){
+
+    return (f1+f2);
+}
+
+console.log(Myfunction(2, 8));
+
+function greet(){
+
+    console.log("hey Folks");
+
+}
+
+greet();
+
